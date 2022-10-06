8136af0fa135f1ec86c7d94eda4be9441e6b7841
rsf91 <43906435+rsf91@users.noreply.github.com>
2022-10-06 20:49:22 +0200

    Add files via upload

---
+++ analise_apuracao.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  5 17:45:44 2022
+
+@author: rodrigosferreira
+"""
+
+import pandas as pd
+import ast
+#import matplotlib.pyplot as plt
+
+def create_graph():
+    data = pd.read_csv('apuracao.txt', sep=";", header=None)
+    
+    results = pd.DataFrame()
+    
+    for i in data.index:
+        data[1][i] = ast.literal_eval(data[1][i])
+    
+        df = pd.DataFrame(
+            [[
+                ast.literal_eval(data[1][i]['LULA']),ast.literal_eval(data[1][i]['JAIR BOLSONARO'])
+                ]], 
+              columns=["LULA","JAIR BOLSONARO"],index=[data[0][i]])
+        
+        results = results.append(df)
+    
+    
+    #Converting to Datetime
+    
+    results.index = pd.to_datetime(results.index)
+    
+    results.plot()
+
+create_graph()
---
+++ apuracao.py
@@ -0,0 +1,108 @@
+
+
+# O código é do @BrennoSullivan, só tô colocando aqui pra ficar mais fácil pro pessoal copiar.
+# Adicionado Loop de 15 segundos e diferença de votos entre top 2.
+
+"""
+
+Coisas que precisam ser ajustadas:
+    
+    o URL do data request
+    O nome de ambos candidatos no analise apuracao
+
+"""
+
+
+import requests
+import json
+import pandas as pd
+from time import sleep
+from datetime import datetime
+# import os
+#import analise_apuracao
+
+x = 0
+
+n = 10
+
+while n > 0:
+
+
+    data = requests.get(
+        'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
+    
+    #O lugar para conseguir esse caminho é no site oficial, F12, TAB NETWORK, procurar pelo termpo .json e achar algo parecido com o valor acima
+    
+    # https://resultados.tse.jus.br/oficial/app/index.html#/eleicao;e=e544;uf=sp;ufbu=sp;mubu=71072;zn=0001;se=0001/resultados/cargo/1
+    
+
+    json_data = json.loads(data.content)
+
+    candidato = []
+    partido = []
+    votos = []
+    porcentagem = []
+
+    total_apurado = json_data['psi']
+    total_seq1 = ''
+    total_seq2 = ''
+
+    for informacoes in json_data['cand']:
+
+        # if informacoes['seq'] == '2' or informacoes['seq'] == '3' or informacoes['seq'] == '4' or informacoes['seq'] == '7':
+            candidato.append(informacoes['nm'])
+            votos.append(informacoes['vap'])
+            porcentagem.append(informacoes['pvap'])
+
+            if informacoes['seq'] == '1':
+                total_seq1 = informacoes['pvap']
+
+            if informacoes['seq'] == '2':
+                total_seq2 = informacoes['pvap']
+
+    # total_diff = float(total_seq1) - float(total_seq2)
+
+
+    df_eleicao = pd.DataFrame(list(zip(candidato, votos, porcentagem)), columns = [
+        'Candidato', 'Nº de Votos', 'Porcentagem'
+    ])
+
+
+    print(f"""
+Total apurado: {total_apurado}%
+{df_eleicao}
+Diff de votos: {float(total_seq1.replace(',', '.')) - float(total_seq2.replace(',', '.'))}
+        """)
+
+    #Aqui a gente cria o tuple de log
+
+    if x==4:
+        
+        log_resultados = str({json_data['cand'][0]['nm']: json_data['cand'][0]['vap'],json_data['cand'][1]['nm']: json_data['cand'][1]['vap']})
+        
+    
+        log_tuple = (str(datetime.utcnow()),
+                     log_resultados,
+                     total_apurado,
+                     "\n"
+                     )
+        
+        #Aqui a gente transforma esse tuple em string pra depois quebrar em grafico
+        
+        log_string = ";".join(log_tuple)    
+        
+        #Aqui é pra escrever o que está acontecendo no log 
+        
+        f = open("apuracao.txt", "a")
+        f.write(log_string)
+        f.close()
+        
+        x=0
+        
+#        analise_apuracao.create_graph()
+
+    sleep(60)
+    
+    x=x+1
+    
+    
